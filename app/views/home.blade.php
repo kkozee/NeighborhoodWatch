83a822ccdc9b4bc2155ee137f0042ebd0a8b9cf7
Kevin Kozee <Kevin@Kevins-MacBook-Pro.local>
2014-11-21 09:58:55 -0500

    Initial commit

---
+++ app/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('templates.main')
+
+@section('content')
+    @extends('templates.login')
+@stop
---
+++ app/views/templates/main.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+@yield('content')
+</body>
+</html>
